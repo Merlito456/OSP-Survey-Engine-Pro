b8bc04693368182fa3ecb23717b5e3fb28c9938b
Merlito456 <rabanes.johncarlo1994@gmail.com>
2026-01-04 00:44:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.js';
+
+// Register Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./sw.js');
+  });
+}
+
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error('Could not find root element to mount to');
+}
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  React.createElement(
+    React.StrictMode,
+    null,
+    React.createElement(App)
+  )
+);
